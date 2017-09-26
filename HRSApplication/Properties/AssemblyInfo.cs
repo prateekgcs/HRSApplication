d8c365a20570a4461055107e87bc330e9b4b98ad
Prateek Gupta <30597387+prateekgcs@users.noreply.github.com>
2017-09-26 17:11:48 +0530

    Updated Project Files

--- HRSApplication/Properties/AssemblyInfo.cs
+++ HRSApplication/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c0f7cad-4f9f-47a2-9c97-a59bd8c4366e")]
+[assembly: Guid("59c02996-9b2d-43a6-a495-5cb3d4a7aff9")]
 
 // Version information for an assembly consists of the following four values:
 //
